0534ec3f011dec0aedae3cdd182886c89da5b3d6
cli1903 <47994462+cli1903@users.noreply.github.com>
2020-06-24 19:59:26 -0400

    move Builder to top of class for TripModel.java

--- frontend/src/main/java/com/google/tripmeout/frontend/TripModel.java
+++ frontend/src/main/java/com/google/tripmeout/frontend/TripModel.java
@@ -4,6 +4,15 @@ import com.google.auto.value.AutoValue;
 
 @AutoValue
 public abstract class TripModel {
+  @AutoValue.Builder
+  public static abstract class Builder {
+    public abstract Builder setId(String id);
+    public abstract Builder setName(String name);
+    public abstract Builder setLocationLat(long latitude);
+    public abstract Builder setLocationLong(long longitude);
+    public abstract TripModel build();
+  }
+  
   public abstract String id();
   public abstract String name();
   public abstract long locationLat();
@@ -13,12 +22,5 @@ public abstract class TripModel {
     return new AutoValue_TripModel.Builder();
   }
 
-  @AutoValue.Builder
-  public static abstract class Builder {
-    public abstract Builder setId(String id);
-    public abstract Builder setName(String name);
-    public abstract Builder setLocationLat(long latitude);
-    public abstract Builder setLocationLong(long longitude);
-    public abstract TripModel build();
-  }
+  
 }

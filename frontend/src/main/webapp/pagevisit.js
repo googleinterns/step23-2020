9019ba48ee8740209d88823cb33a86aea9dfddc9
Charles Lovett <calovett35@gmail.com>
2020-06-25 14:46:07 +0000

    .

---
+++ frontend/src/main/webapp/pagevisit.js
@@ -0,0 +1,26 @@
+function PageData() {
+  var visit = {
+    'name': 'Big Ben',
+    'distance': 1,
+    'description':
+        'It is a big clock tower that is currently under contruction',
+    'image': 'image here'
+  };
+  createVisitElement(visit);
+}
+
+function createVisitElement(visit) {
+  const nameContainer = document.getElementById('name');
+
+  nameContainer.appendChild(createNameElement(visit));
+  const descriptionContainer = document.getElementById('description');
+  descriptionContainer.innerText = visit.description;
+  const imageContainer = document.getElementById('image');
+  imageContainer.innerText = visit.image;
+}
+
+function createNameElement(place) {
+  const nameElement = document.createElement('span');
+  nameElement.innerText = place.name + ' Distance: ' + place.distance + 'mi';
+  return nameElement;
+}

72a7eab6557d0c193e465c65e2153dd5a86bbc2c
Cindy Li <cindy_li@brown.edu>
2020-06-26 21:59:31 +0000

    fix rebasing issues

--- frontend/src/main/java/com/google/tripmeout/frontend/PlaceVisitStorage.java
+++ frontend/src/main/java/com/google/tripmeout/frontend/PlaceVisitStorage.java
@@ -1,17 +1,10 @@
 package com.google.tripmeout.frontend;
 
-<<<<<<< HEAD
 import com.google.tripmeout.frontend.PlaceVisitModel;
 
-=======
->>>>>>> e878473... create PlaceVisit and Trip Storage interfaces
 public interface PlaceVisitStorage {
   public void addPlaceVisit(PlaceVisitModel placeVisit);
   public void removePlaceVisit(String tripId, String placeId);
   public PlaceVisitModel getPlaceVisit(String tripId, String placeId);
   public void changePlaceVisitStatus(String tripId, String placeId, String newStatus);
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> e878473... create PlaceVisit and Trip Storage interfaces

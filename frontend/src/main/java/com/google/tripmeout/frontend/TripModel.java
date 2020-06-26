26f155ce7b8b5263c0e3ba3415fa6ebc228e43fc
Cindy Li <cindy_li@brown.edu>
2020-06-26 21:21:52 +0000

    add userId field to trip

--- frontend/src/main/java/com/google/tripmeout/frontend/TripModel.java
+++ frontend/src/main/java/com/google/tripmeout/frontend/TripModel.java
@@ -10,6 +10,7 @@ public abstract class TripModel {
   
   public abstract String id();
   public abstract String name();
+  public abstract String userId();
   public abstract float locationLat();
   public abstract float locationLong();
   
@@ -17,6 +18,7 @@ public abstract class TripModel {
   public static abstract class Builder {
     public abstract Builder setId(String id);
     public abstract Builder setName(String name);
+    public abstract Builder setUserId(String userId);
     public abstract Builder setLocationLat(float latitude);
     public abstract Builder setLocationLong(float longitude);
     public abstract TripModel build();

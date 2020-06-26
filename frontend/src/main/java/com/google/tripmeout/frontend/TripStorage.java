ffb0b2edc489f2ae7538eb9ca3a3b43c53e7a5b9
Cindy Li <cindy_li@brown.edu>
2020-06-26 21:20:08 +0000

    create PlaceVisit and Trip Storage interfaces

---
+++ frontend/src/main/java/com/google/tripmeout/frontend/TripStorage.java
@@ -0,0 +1,13 @@
+package com.google.tripmeout.frontend;
+
+import com.google.tripmeout.frontend.TripModel;
+import com.google.tripmeout.frontend.PlaceVisitModel;
+import java.util.List;
+
+public interface TripStorage {
+  public void addTrip(TripModel trip);
+  public void removeTrip(String tripId);
+  public TripModel getTrip(String tripId);
+  public List<TripModel> getAllUserTrips(String userId);
+  public List<PlaceVisitModel> getTripPlaceVisits(String tripId);
+}

7badecf2dc3730404d113d981f31cdd49e41f7d2
Cindy Li <cindy_li@brown.edu>
2020-06-29 15:02:57 +0000

    add javadoc comments, move storage interfaces to storage folder

---
+++ frontend/src/main/java/com/google/tripmeout/frontend/storage/PlaceVisitStorage.java
@@ -0,0 +1,62 @@
+package com.google.tripmeout.frontend.storage;
+
+import com.google.tripmeout.frontend.PlaceVisitModel;
+
+/**
+ * stores PlaceVisit objects
+ */
+public interface PlaceVisitStorage {
+  /**
+   * adds a PlaceVisitModel object to storage
+   * 
+   * @param placeVisit the PlaceVisitModel object to add to storage
+   *
+   * @throws a PlaceAlreadyExists exception if there is a PlaceVisitModel in 
+   * storage with the same tripId and placeId as placeVisit
+   */
+  public void addPlaceVisit(PlaceVisitModel placeVisit);
+
+  /**
+   * removes from storage the PlaceVisitModel whose tripId and placeId match the
+   * given tripId and placeId, respectively
+   *
+   * @param tripId the id to match PlaceVisitModel object's placeId field on
+   * @param placeId the id to match PlaceVisitModel object's placeId field on
+   *
+   * @throws a PlaceVisitNotFound exception if there is no PlaceVisitModel 
+   * object in storage with the given tripId and placeId 
+   */
+  public void removePlaceVisit(String tripId, String placeId);
+
+  /**
+   * returns the PlaceVisitModel whose tripId and placeId match the given tripId
+   * and placeId, respectively
+   *
+   * @param tripId the id to match PlaceVisitModel object's placeId field on
+   * @param placeId the id to match PlaceVisitModel object's placeId field on
+   *
+   * @throws a PlaceVisitNotFound exception if there is no PlaceVisitModel 
+   * object in storage with the given tripId and placeId 
+   */
+  public PlaceVisitModel getPlaceVisit(String tripId, String placeId);
+
+  /**
+   * updates the userMark parameter of the PlaceVisitModel object whose tripId 
+   * and placeId match the given tripId and placeId, respectively
+   *
+   * @param tripId the id to match PlaceVisitModel object's tripId field on
+   * @param placeId the id to match PlaceVisitModel object's placeId field on
+   *
+   * @throws a PlaceVisitNotFound exception if there is no PlaceVisitModel 
+   * object in storage with the given tripId and placeId 
+   */
+  public void changePlaceVisitStatus(String tripId, String placeId, String newStatus);
+
+  /**
+   * gets a list of all of the PlaceVisitModel objects whose tripId matches the 
+   * given tripId
+   *
+   * @param tripId the id to match PlaceVisitModel object's placeId field on
+   */
+  public List<PlaceVisitModel> getTripPlaceVisits(String tripId);
+}

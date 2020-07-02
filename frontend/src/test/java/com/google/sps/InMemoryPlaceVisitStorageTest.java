3acaffc14b9daffe7a1f07344a78517310ff6c7c
Cindy Li <cindy_li@brown.edu>
2020-07-02 00:18:13 +0000

    add comments to testing

--- frontend/src/test/java/com/google/sps/InMemoryPlaceVisitStorageTest.java
+++ frontend/src/test/java/com/google/sps/InMemoryPlaceVisitStorageTest.java
@@ -124,7 +124,7 @@ public final class InMemoryPlaceVisitStorageTest {
   }
 
   /**
-   * test that a PlaceNotFoundException is thrown when removing a place from
+   * test that a PlaceNotFoundException is thrown when getting a place from
    * an empty storage
    */
   @Test(expected = PlaceVisitNotFoundException.class)
@@ -133,8 +133,9 @@ public final class InMemoryPlaceVisitStorageTest {
   }
 
   /**
-   * test that a PlaceNotFoundException is thrown when removing a place from
-   *  
+   * test that a PlaceNotFoundException is thrown when trying to get a place
+   * from storage when there is no place in storage with the given tripId and
+   * placeId
    */
   @Test(expected = PlaceVisitNotFoundException.class)
   public void testGetPlace_notInStorage() throws PlaceVisitAlreadyExistsException, PlaceVisitNotFoundException {
@@ -148,6 +149,10 @@ public final class InMemoryPlaceVisitStorageTest {
     storage.getPlaceVisit(SEOUL.tripId(), SEOUL.placeId());
   }
 
+  /**
+   * test getPlaceVisit when PlaceVisitModel object with given tripId and 
+   * placeId exist in storage
+   */
   @Test
   public void testAddPlace_getPlace() throws PlaceVisitAlreadyExistsException, PlaceVisitNotFoundException {
     InMemoryPlaceVisitStorage storage = new InMemoryPlaceVisitStorage();
@@ -162,11 +167,20 @@ public final class InMemoryPlaceVisitStorageTest {
 
   }
 
+  /**
+   * test that a PlaceNotFoundException is thrown when removing a place from
+   * an empty storage
+   */
   @Test(expected = PlaceVisitNotFoundException.class)
   public void testRemovePlace_emptyStorage() throws PlaceVisitNotFoundException {
     NO_PLACE_VISITS.removePlaceVisit(TOKYO.tripId(), TOKYO.placeId());
   }
 
+  /**
+   * test that a PlaceNotFoundException is thrown when trying to remove a place
+   * from storage when there is no place in storage with the given tripId and
+   * placeId
+   */
   @Test(expected = PlaceVisitNotFoundException.class)
   public void testremovePlace_notInStorage() throws PlaceVisitAlreadyExistsException, PlaceVisitNotFoundException {
     InMemoryPlaceVisitStorage storage = new InMemoryPlaceVisitStorage();
@@ -179,6 +193,28 @@ public final class InMemoryPlaceVisitStorageTest {
     storage.removePlaceVisit(SEOUL.tripId(), SEOUL.placeId());
   }
 
+  /**
+   * test that no exception is thrown when removing a place from storage when 
+   * there is a place in storage with the given tripId and placeId
+   */
+  @Test
+  public void testremovePlace_noException() throws PlaceVisitAlreadyExistsException, PlaceVisitNotFoundException {
+    InMemoryPlaceVisitStorage storage = new InMemoryPlaceVisitStorage();
+    storage.addPlaceVisit(LONDON);
+    storage.addPlaceVisit(PARIS);
+    storage.addPlaceVisit(ROME);
+    storage.addPlaceVisit(BEIJING);
+    storage.addPlaceVisit(TOKYO);
+    storage.addPlaceVisit(TOKYO_B);
+    storage.removePlaceVisit(ROME.tripId(), ROME.placeId());
+  }
+
+  /**
+   * test that place is actually removed when removing a place from storage when 
+   * there is a place in storage with the given tripId and placeId by checking
+   * that calling getPlaceVisit on the removed PlaceVisit tripId and placeId
+   * results in PlaceVisitNotFoundException being thrown
+   */
   @Test(expected = PlaceVisitNotFoundException.class)
   public void testremovePlace_getRemovedPlace() throws PlaceVisitAlreadyExistsException, PlaceVisitNotFoundException {
     InMemoryPlaceVisitStorage storage = new InMemoryPlaceVisitStorage();
@@ -192,6 +228,10 @@ public final class InMemoryPlaceVisitStorageTest {
     storage.getPlaceVisit(ROME.tripId(), ROME.placeId());
   }
 
+  /**
+   * test that changePlaceVisitStatus returns true and changes userMark field
+   * when PlaceVisitModel object with the given tripId and placeId is in storage
+   */
   @Test
   public void testChangeStatus_inStorage() throws PlaceVisitAlreadyExistsException, PlaceVisitNotFoundException {
     InMemoryPlaceVisitStorage storage = new InMemoryPlaceVisitStorage();
@@ -205,6 +245,10 @@ public final class InMemoryPlaceVisitStorageTest {
     Assert.assertEquals(changedParis.userMark(), "don't-care");
   }
 
+  /**
+   * test that changePlaceVisitStatus returns false when PlaceVisitModel object
+   * with the given tripId and placeId is not in storage
+   */
   @Test
   public void testChangeStatus_notInStorage() throws PlaceVisitAlreadyExistsException, PlaceVisitNotFoundException {
     InMemoryPlaceVisitStorage storage = new InMemoryPlaceVisitStorage();
@@ -216,6 +260,11 @@ public final class InMemoryPlaceVisitStorageTest {
     Assert.assertFalse(response);
   }
 
+  /**
+   * test that changePlaceVisitStatus returns true and userMark field stays same
+   * when changing status to same status as original PlaceVisitModel
+   * when PlaceVisitModel object with the given tripId and placeId is in storage
+   */
   @Test
   public void testChangeStatusSame_inStorage() throws PlaceVisitAlreadyExistsException, PlaceVisitNotFoundException {
     InMemoryPlaceVisitStorage storage = new InMemoryPlaceVisitStorage();
@@ -229,6 +278,11 @@ public final class InMemoryPlaceVisitStorageTest {
     Assert.assertEquals(changedParis.userMark(), "if-time");
   }
 
+  /**
+   * test that only the PlaceVisitModel objects with the given tripId and 
+   * userMark of "must-see" or "if-time" are returned in list when there is only
+   * one unique tripId in storage
+   */
   @Test
   public void testListPlaces_oneTripInStorage() throws PlaceVisitAlreadyExistsException {
     InMemoryPlaceVisitStorage storage = new InMemoryPlaceVisitStorage();
@@ -240,6 +294,11 @@ public final class InMemoryPlaceVisitStorageTest {
     assertThat(tripAPlaces).containsExactly(ROME, TOKYO, PARIS);
   }
 
+  /**
+   * test that only the PlaceVisitModel objects with the given tripId and 
+   * userMark of "must-see" or "if-time" are returned in list when there are
+   * multiple unique tripIds in storage
+   */
   @Test
   public void testListPlaces_multipleTripsInStorage() throws PlaceVisitAlreadyExistsException {
     InMemoryPlaceVisitStorage storage = new InMemoryPlaceVisitStorage();
@@ -254,6 +313,10 @@ public final class InMemoryPlaceVisitStorageTest {
     assertThat(tripAPlaces).containsExactly(ROME, TOKYO, PARIS);
   }
 
+  /**
+   * test that no PlaceVisitModel objects are returned in list when there are no
+   * PlaceVisitModel objects in storage with the given tripId
+   */
   @Test
   public void testListPlaces_tripNotInStorage() throws PlaceVisitAlreadyExistsException {
     InMemoryPlaceVisitStorage storage = new InMemoryPlaceVisitStorage();

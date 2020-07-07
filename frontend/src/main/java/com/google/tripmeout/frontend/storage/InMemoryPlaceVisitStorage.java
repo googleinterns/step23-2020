48773a57bb94aee285d8b204f81de2319d4ec8f8
Cindy Li <cindy_li@brown.edu>
2020-07-07 19:47:11 +0000

    add synchronized blocks

--- frontend/src/main/java/com/google/tripmeout/frontend/storage/InMemoryPlaceVisitStorage.java
+++ frontend/src/main/java/com/google/tripmeout/frontend/storage/InMemoryPlaceVisitStorage.java
@@ -16,33 +16,37 @@ import java.util.Map;
 import java.util.Optional;
 
 public class InMemoryPlaceVisitStorage implements PlaceVisitStorage {
-  // <placeId, tripId, PlaceVisitModel> 
+  // <tripId, placeId, PlaceVisitModel> 
   Map<String, Map<String, PlaceVisitModel>> storage = new ConcurrentHashMap<>();
  
   @Override
   public void addPlaceVisit(PlaceVisitModel placeVisit) throws PlaceVisitAlreadyExistsException {
-    Map<String, PlaceVisitModel> tripsMap = storage.get(placeVisit.placeId());
+    synchronized (storage) {
+      Map<String, PlaceVisitModel> placesMap = storage.get(placeVisit.tripId());
 
-    if (tripsMap == null) {
-      Map<String, PlaceVisitModel> newPlaceMap = new ConcurrentHashMap<>();
-      newPlaceMap.put(placeVisit.tripId(), placeVisit);
-      storage.put(placeVisit.placeId(), newPlaceMap);
+      if (placesMap == null) {
+        Map<String, PlaceVisitModel> newPlaceMap = new ConcurrentHashMap<>();
+        newPlaceMap.put(placeVisit.placeId(), placeVisit);
+        storage.put(placeVisit.tripId(), newPlaceMap);
 
-    } else if (tripsMap.get(placeVisit.tripId()) == null) {
-      tripsMap.put(placeVisit.tripId(), placeVisit);
+      } else if (placesMap.get(placeVisit.placeId()) == null) {
+        placesMap.put(placeVisit.placeId(), placeVisit);
 
-    } else {
-      throw new PlaceVisitAlreadyExistsException("PlaceVisit " + 
-        placeVisit.name() + " already exists for trip " + placeVisit.tripId());
+      } else {
+        throw new PlaceVisitAlreadyExistsException("PlaceVisit " + 
+          placeVisit.name() + " already exists for trip " + placeVisit.tripId());
+      }
     }
   }
 
   @Override
   public void removePlaceVisit(String tripId, String placeId) throws PlaceVisitNotFoundException {
-    Map<String, PlaceVisitModel> tripsMap = storage.get(placeId);
-    if (tripsMap == null || tripsMap.remove(tripId) == null) {
-      throw new PlaceVisitNotFoundException("PlaceVisit with id" + placeId + 
+    synchronized (storage) {
+      Map<String, PlaceVisitModel> placesMap = storage.get(tripId);
+      if (placesMap == null || placesMap.remove(placeId) == null) {
+        throw new PlaceVisitNotFoundException("PlaceVisit with id" + placeId + 
           " not found for trip " + tripId);
+      }
     }
   }
 
@@ -57,28 +61,32 @@ public class InMemoryPlaceVisitStorage implements PlaceVisitStorage {
 
   @Override
   public boolean changePlaceVisitStatus(String tripId, String placeId, String newStatus) {
-    Map<String, PlaceVisitModel> tripsMap = storage.get(placeId);
-    if (tripsMap == null || tripsMap.get(tripId) == null) {
-      return false;
-    } else {
-      PlaceVisitModel place = tripsMap.get(tripId);
-      PlaceVisitModel updatedPlace = place.toBuilder().setUserMark(newStatus).build();
-      tripsMap.put(tripId, updatedPlace);
-      return true;
+    synchronized (storage) {
+      Map<String, PlaceVisitModel> placesMap = storage.get(tripId);
+      if (placesMap == null || placesMap.get(placeId) == null) {
+        return false;
+      } else {
+        PlaceVisitModel place = placesMap.get(placeId);
+        PlaceVisitModel updatedPlace = place.toBuilder().setUserMark(newStatus).build();
+        placesMap.put(placeId, updatedPlace);
+        return true;
+      }
     }
   }
 
   @Override
   public List<PlaceVisitModel> getTripPlaceVisits(String tripId) {
+    Map<String, PlaceVisitModel> placesMap = storage.get(tripId);
     List<PlaceVisitModel> tripPlaceVisits = new ArrayList<>();
-    storage.forEach((placeId, tripIdMap) -> {
-      PlaceVisitModel place = tripIdMap.get(tripId);
+    if (placesMap == null) {
+      return tripPlaceVisits;
+    }
+    for (PlaceVisitModel place: placesMap.values()) {
       if (place != null && (place.userMark().equals("must-see") || place.userMark().equals("if-time"))) {
         tripPlaceVisits.add(place);
       }
-    });
+    }
 
     return tripPlaceVisits;
   }
- 
 }

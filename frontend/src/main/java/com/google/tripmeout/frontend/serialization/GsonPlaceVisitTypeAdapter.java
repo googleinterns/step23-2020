b08c6eaa6665f5ba0634a572e8f663c48f95a159
chuckluv <calovett35@gmail.com>
2020-07-08 19:27:13 +0000

    create Place Visit Type adapter

---
+++ frontend/src/main/java/com/google/tripmeout/frontend/serialization/GsonPlaceVisitTypeAdapter.java
@@ -0,0 +1,16 @@
+package com.google.tripmeout.frontend.serialization;
+
+import com.google.common.base.Strings;
+import com.google.gson.JsonParseException;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
+import com.google.tripmeout.frontend.TripModel;
+import java.io.IOException;
+
+public class GsonPlaceVisitTypeAdapter extends TypeAdapter<TripModel> {
+    
+
+    
+}

8aac4f430016da9bff42e388e657295b102fe727
Chris Bowzer <chris.j.bowzer@gmail.com>
2020-07-13 12:54:52 -0700

    Add a default ThemeData object

---
+++ frontend/ui/lib/themes/default_theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+final ThemeData defaultTheme = ThemeData(
+  // TODO: customize me...
+  primaryColor: const Color(0xff526948),
+  accentColor: const Color(0xff6fa15a),
+);

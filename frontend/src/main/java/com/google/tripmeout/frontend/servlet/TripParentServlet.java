8a8808b09c34dac621726fc36d679189a113c263
chuckluv <calovett35@gmail.com>
2020-07-16 16:53:06 +0000

    save

--- frontend/src/main/java/com/google/tripmeout/frontend/servlet/TripParentServlet.java
+++ frontend/src/main/java/com/google/tripmeout/frontend/servlet/TripParentServlet.java
@@ -1,9 +1,15 @@
 package com.google.tripmeout.frontend.servlet;
 
-import com.google.tripmeout.frontend.storage.TripStorage;
-import com.google.tripmeout.frontend.TripModel;
+import com.google.appengine.repackaged.com.google.api.client.util.Strings;
+import com.google.common.flogger.FluentLogger;
 import com.google.gson.Gson;
+import com.google.gson.JsonParseException;
+import com.google.tripmeout.frontend.TripModel;
+import com.google.tripmeout.frontend.error.TripAlreadyExistsException;
+import com.google.tripmeout.frontend.storage.TripStorage;
 import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import javax.inject.Inject;
@@ -11,50 +17,66 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class TripParentServlet extends HttpServlet{
+public class TripParentServlet extends HttpServlet {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+  private static final String TRIP_PARENT_PARAM_KEY = "name";
+  private static final String LONG_PARAM_KEY = "locationLong";
+  private static final String LAT_PARAM_KEY = "locationLat";
+  private static final String USER_ID_PARAM_KEY = "userId";
+  private final Gson gson;
+  private final TripStorage storage;
 
-    private static final String TRIP_PARENT_PARAM_KEY="tripName";
-    private static final String LONG_PARAM_KEY="long";
-    private static final String LAT_PARAM_KEY="lat";
-    private static final String USER_ID_PARAM_KEY="userId";
-    private final Gson gson;
-    private final TripStorage storage;
+  @Inject
+  public TripParentServlet(TripStorage storage, Gson gson) {
+    this.gson = gson;
+    this.storage = storage;
+  }
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String userId = getUserId();
+    response.setContentType("application/json");
+    response.getWriter().print(gson.toJson(storage.getAllUserTrips(userID)));
+  }
+  private String getUserId(){
+      return "fakeUser";
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    logger.atInfo().log("received doPost");
+      String userId = getUserId();
+      Optional<TripModel> requestTrip = extractFromRequestBody(request, gson, TripModel.class);
+     
+      if (requestTrip.isPresent()) {
+        try {
+          storage.addTrip(requestTrip.get());
+          response.setContentType("application/json");
+          response.getWriter().print(gson.toJson(requestTrip.get()));
+        } catch (TripAlreadyExistsException e) {
+          response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
+
+      } else {
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+      }
+
     
-    @Inject
-    public TripParentServlet(TripStorage storage, Gson gson){
-        this.gson = gson;
-        this.storage = storage;
+  }
+
+private static TripModel resolveDefault(){
+    return TripModel.builder().setId(UUID.randomUUID().toString()).build();
+}
+
+  //TODO: use servletutil onced pushed
+  private static <T> Optional<T> extractFromRequestBody(
+      HttpServletRequest request, Gson gson, Class<T> clazz) throws IOException {
+    try (Reader reader = request.getReader()) {
+      return Optional.ofNullable(gson.fromJson(reader, clazz));
+    } catch (JsonParseException e) {
+      logger.atWarning().withCause(e).log(
+          "received exception while attempting to parse json from reader!");
+      return Optional.empty();
     }
-
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String userID = request.getParameter(USER_ID_PARAM_KEY);
-        response.setContentType("application/json") ; 
-        response.getWriter().print(gson.toJson(storage.getAllUserTrips(userID)));
-    }
-
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {  
-        String tripName = request.getParameter(TRIP_PARENT_PARAM_KEY);
-        String longitude = request.getParameter(LONG_PARAM_KEY);
-        String latitude = request.getParameter(LAT_PARAM_KEY);
-        String userID = request.getParameter(USER_ID_PARAM_KEY);
-       
-        TripModel trip = TripModel.builder()
-        .setId(UUID.randomUUID().toString())
-        .setName(tripName)
-        .setUserId(userID)
-        .setLocationLat(Double.parseDouble(latitude))
-        .setLocationLong(Double.parseDouble(longitude))
-        .build();
-
-        storage.addTrip(trip);
-        
-        response.getWriter().print(gson.toJson(trip));
-
-
-
-
-    }
-
+  }
 }
--- frontend/src/test/java/com/google/sps/TripParentServletTest.java
+++ frontend/src/test/java/com/google/sps/TripParentServletTest.java
@@ -5,13 +5,14 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
-import com.google.tripmeout.frontend.storage.TripStorage;
-import com.google.tripmeout.frontend.TripModel;
-import com.google.tripmeout.frontend.servlet.TripParentServlet;
 import com.google.common.collect.ImmutableList;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import java.io.BufferedReader;
+import com.google.gson.reflect.TypeToken;
+import com.google.tripmeout.frontend.TripModel;
+import com.google.tripmeout.frontend.serialization.GsonTripModelTypeAdapter;
+import com.google.tripmeout.frontend.servlet.TripParentServlet;
+import com.google.tripmeout.frontend.storage.TripStorage;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringReader;
@@ -28,34 +29,129 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 
 @RunWith(JUnit4.class)
-public class TripParentServletTest{
-    @Mock TripStorage storage;
-    @Mock HttpServletRequest request;
-    @Mock HttpServletResponse response;
+public class TripParentServletTest {
+  @Mock TripStorage storage;
+  @Mock HttpServletRequest request;
+  @Mock HttpServletResponse response;
 
-    Gson gson;
-    TripParentServlet tripParentServlet;
+  Gson gson;
+  TripParentServlet tripParentServlet;
 
+  @Before
+  public void setup() {
+    initMocks(this);
+    gson = new GsonBuilder()
+               .registerTypeAdapter(TripModel.class, new GsonTripModelTypeAdapter())
+               .create();
+    tripParentServlet = new TripParentServlet(storage, gson);
+  }
 
-    @Before
-    public void setup(){
-        initMocks(this);
-        gson =
-            new GsonBuilder().registerTypeAdapter(TripModel.class, new JsonSerlializer<TripModel>()).create();
-        tripParentServlet = new TripParentServlet(storage, gson);
-    }
+  @Test
+  public void doGet_noTrips() throws Exception {
+    when(storage.getAllUserTrips("user1")).thenReturn(ImmutableList.of());
+    StringWriter responseCapture = new StringWriter();
+    when(response.getWriter()).thenReturn(new PrintWriter(responseCapture));
 
+    tripParentServlet.doGet(request, response);
 
-    @Test
-    public void doGet_noTrips() throws Exception{
-        when(storage.getAllUserTrips("user1")).thenReturn(ImmutableList.of());
-        StringWriter responseCapture = new StringWriter();
-        when(response.getWriter()).thenReturn(new PrintWriter(responseCapture));
+    verify(response).setContentType("application/json");
+    assertThat(responseCapture.toString()).isEqualTo("[]");
+  }
+  @Test
+  public void doGet_someTrips() throws Exception {
+    TripModel trip1 = createTripWithUserId("user1");
+    TripModel trip2 = createTripWithUserId("user1");
+    when(storage.getAllUserTrips("user1")).thenReturn(ImmutableList.of(trip1, trip2));
+    StringWriter responseCapture = new StringWriter();
+    when(response.getWriter()).thenReturn(new PrintWriter(responseCapture));
+    tripParentServlet.doGet(request, response);
+    verify(response).setContentType("application/json");
+    List<TripModel> tripResponse =
+        gson.fromJson(responseCapture.toString(), new TypeToken<List<TripModel>>() {}.getType());
+    assertThat(tripResponse).containsExactly(trip1, trip2);
+  }
 
-        tripParentServlet.doGet(request, response);
+  @Test
+  public void doPost_ioExceptionWhenReadingRequest_internalServerErrorResponse() throws Exception {
+    when(request.getParameter("name")).thenThrow(new IOException("something bad happened"));
+    when(request.getContentType()).thenReturn("application/json;");
 
-        verify(response).setContentType("application/json;");
-         assertThat(responseCapture.toString()).isEqualTo("[]");
-    }
+    tripParentServlet.doPost(request, response);
 
+    verify(response).setStatus(500);
+  }
+
+  @Test
+  public void doPost_validTripJson() throws Exception {
+    TripModel trip = createTripWithUserId("user1");
+    when(request.getParameter("name")).thenReturn("name");
+    when(request.getParameter("locationLong")).thenReturn("44.2");
+    when(request.getParameter("locationLat")).thenReturn("50.3");
+    when(request.getParameter("userId")).thenReturn("user1");
+    when(request.getContentType()).thenReturn("application/json;");
+    StringWriter responseCapture = new StringWriter();
+    when(response.getWriter()).thenReturn(new PrintWriter(responseCapture));
+
+    tripParentServlet.doPost(request, response);
+    verify(response).setContentType("application/json");
+    TripModel responseTrip = gson.fromJson(responseCapture.toString(), TripModel.class);
+    ArgumentCaptor<TripModel> storageCaptor = ArgumentCaptor.forClass(TripModel.class);
+    verify(storage).addTrip(storageCaptor.capture());
+    assertThat(responseTrip).isEqualTo(storageCaptor.getValue());
+    assertThat(responseTrip.id()).isNotEmpty();
+    assertThat(responseTrip.userId()).isEqualTo(trip.userId());
+    assertThat(responseTrip.name()).isEqualTo(trip.name());
+    assertThat(responseTrip.locationLat()).isEqualTo(trip.locationLat());
+    assertThat(responseTrip.locationLong()).isEqualTo(trip.locationLong());
+  }
+  @Test
+  public void doPost_invalidTrip_NoTripName() throws Exception {
+    when(request.getParameter("locationLong")).thenReturn("44.2");
+    when(request.getParameter("locationLat")).thenReturn("50.3");
+    when(request.getParameter("userId")).thenReturn("user1");
+    tripParentServlet.doPost(request, response);
+    verify(response).setStatus(400);
+  }
+
+  @Test
+  public void doPost_invalidTrip_NoUserId() throws Exception {
+    when(request.getParameter("name")).thenReturn("name");
+    when(request.getParameter("locationLong")).thenReturn("44.2");
+    when(request.getParameter("locationLat")).thenReturn("50.3");
+    tripParentServlet.doPost(request, response);
+    verify(response).setStatus(400);
+  }
+  @Test
+  public void doPost_invalidTrip_NoLongitude() throws Exception {
+    when(request.getParameter("name")).thenReturn("name");
+    when(request.getParameter("locationLat")).thenReturn("50.3");
+    when(request.getParameter("userId")).thenReturn("user1");
+    tripParentServlet.doPost(request, response);
+    verify(response).setStatus(400);
+  }
+  @Test
+  public void doPost_invalidTrip_NoLatitude() throws Exception {
+    when(request.getParameter("name")).thenReturn("name");
+    when(request.getParameter("locationLong")).thenReturn("44.2");
+    when(request.getParameter("userId")).thenReturn("user1");
+    tripParentServlet.doPost(request, response);
+    verify(response).setStatus(400);
+  }
+
+  @Test
+  public void doPost_noContentTypeSet_badRequestResponse() throws Exception {
+    tripParentServlet.doPost(request, response);
+
+    verify(response).setStatus(400);
+  }
+
+  private static TripModel createTripWithUserId(String userId) {
+    return TripModel.builder()
+        .setId("id")
+        .setName("name")
+        .setUserId(userId)
+        .setLocationLat(50.3)
+        .setLocationLong(44.2)
+        .build();
+  }
 }

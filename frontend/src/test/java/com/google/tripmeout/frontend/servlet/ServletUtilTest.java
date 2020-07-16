876bda53c22d7555817b00936b74160dc4d0e089
cli1903 <cinli@google.com>
2020-07-16 00:20:48 +0000

    add testing for servletUtil class

--- frontend/src/test/java/com/google/tripmeout/frontend/servlet/ServletUtilTest.java
+++ frontend/src/test/java/com/google/tripmeout/frontend/servlet/ServletUtilTest.java
@@ -15,70 +15,138 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import com.google.common.io.PatternFilenameFilter;
 import com.google.gson.Gson;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 import com.google.tripmeout.frontend.PlaceVisitModel;
 import com.google.tripmeout.frontend.TripModel;
-import com.google.tripmeout.frontend.error.TripMeOutException;
-import com.google.tripmeout.frontend.servlet.FakeHttpServletResponse;
+import com.google.tripmeout.frontend.serialization.GsonModelSerializationModule;
 import com.google.tripmeout.frontend.servlets.ServletUtil;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.junit.runners.JUnit4;
+import org.junit.runner.RunWith;
 
+@RunWith(JUnit4.class)
 public class ServletUtilTest {
-    @Mock HttpServletRequest request;
-    private Gson gson;
+  @Mock HttpServletRequest request;
+  private Gson gson;
 
-    private final Pattern URI_PATTERN = Pattern.compile(".*/trips/([^/]+)");
+  private final Pattern TRIP_URI_PATTERN = Pattern.compile(".*/trips/([^/]+)");
+  private final Pattern PLACE_URI_PATTERN = Pattern.compile(".*/trips/([^/]+)/places/([^/]+)");
 
-    @Before
-    public void setup() {
-        initMocks(this);
-        this.gson = new Gson();
-    }
+  @Before
+  public void setup() {
+    initMocks(this);
+    Injector injector = Guice.createInjector(new GsonModelSerializationModule());
+    this.gson = injector.getInstance(Gson.class);
+  }
 
-    @Test
-    public void extractFromRequestBody_supplyListClass_returnsOptionalList() throws IOException {
-        when(request.getReader()).thenReturn(new BufferedReader(new StringReader("['hello', 'my', 'name', 'is']")));
+  @Test
+  public void extractFromRequestBody_supplyListClass_returnsOptionalList() throws IOException {
+    when(request.getReader())
+        .thenReturn(new BufferedReader(new StringReader("['hello', 'my', 'name', 'is']")));
 
-        Optional<List> extractedList = ServletUtil.extractFromRequestBody(request, gson, List.class);
-        assertThat(extractedList).isPresent();
-        assertThat(extractedList.get()).containsExactly("hello", "my", "name", "is");
-    }
+    Optional<List> extractedList = ServletUtil.extractFromRequestBody(request, gson, List.class);
+    assertThat(extractedList).isPresent();
+    assertThat(extractedList.get()).containsExactly("hello", "my", "name", "is");
+  }
 
-    @Test
-    public void extractFromRequestBody_supplyTripObject_returnsOptionalTrip() throws IOException {
-        when(request.getReader()).thenReturn(new BufferedReader(new StringReader("{id: abc1234, name: trip1, userId: a, locationLat: 33, locationLong: -22")));
+  @Test
+  public void extractFromRequestBody_supplyTripObject_returnsOptionalTrip() throws IOException {
+    when(request.getReader())
+        .thenReturn(new BufferedReader(new StringReader(
+            "{id: abc123, name: trip1, userId: a, locationLat: 33.2, locationLong: -22.77}")));
 
-        TripModel testTrip = TripModel.builder()
-          .setId("abc123")
-          .setName("trip1")
-          .setUserId("a")
-          .setLocationLat(33)
-          .setLocationLong(-22)
-          .build();
+    TripModel testTrip = TripModel.builder()
+                             .setId("abc123")
+                             .setName("trip1")
+                             .setUserId("a")
+                             .setLocationLat(33.2)
+                             .setLocationLong(-22.77)
+                             .build();
 
-        assertThat(ServletUtil.extractFromRequestBody(request, gson, TripModel.class)).hasValue(testTrip);
-    }
+    assertThat(ServletUtil.extractFromRequestBody(request, gson, TripModel.class))
+        .hasValue(testTrip);
+  }
 
-    @Test
-    public void parseUri_noMatches_returnsEmptyMatcher() {
-        when(request.getRequestURI()).thenReturn("http://tripmeout.com/trip/abc123");
+  @Test
+  public void extractFromRequestBody_supplyTripObjectNotAllFields_returnsOptionalTrip()
+      throws IOException {
+    when(request.getReader())
+        .thenReturn(new BufferedReader(new StringReader("{id: abc123, userId: a}")));
 
-        assertThrows(IllegalArgumentException.class, () -> ServletUtil.parseUri(request, URI_PATTERN));
-    }
+    TripModel testTrip = TripModel.builder().setId("abc123").setUserId("a").build();
 
-    @Test
-    public void parseUri_matches_matcherHasCorrectGroup() {
-        when(request.getRequestURI()).thenReturn("http://tripmeout.com/trips/abc123");
+    assertThat(ServletUtil.extractFromRequestBody(request, gson, TripModel.class))
+        .hasValue(testTrip);
+  }
 
-        Matcher matcher = ServletUtil.parseUri(request, URI_PATTERN);
+  @Test
+  public void extractFromRequestBody_missingId_returnsOptionalTrip() throws IOException {
+    when(request.getReader())
+        .thenReturn(new BufferedReader(
+            new StringReader("{name: trip1, userId: a, locationLat: 33.2, locationLong: -22.77}")));
+    assertThat(ServletUtil.extractFromRequestBody(request, gson, TripModel.class)).isEmpty();
+  }
 
-        assertThat(matcher.matches()).isTrue();
-        assertThat(matcher.group(1)).isEqualTo("abc123");
-    }
+  @Test
+  public void extractFromRequestBody_supplyPlaceVisitObject_returnsOptionalPlaceVisitModel()
+      throws IOException {
+    when(request.getReader())
+        .thenReturn(new BufferedReader(new StringReader(
+            "{tripId: abc123, placeId: 123, name: trip1, userMark: YES, latitude: 33.2, longitude: -22.77}")));
+
+    PlaceVisitModel testPlace1 = PlaceVisitModel.builder()
+                                     .setTripId("abc123")
+                                     .setPlaceId("123")
+                                     .setName("trip1")
+                                     .setUserMark(PlaceVisitModel.UserMark.YES)
+                                     .setLatitude(33.2)
+                                     .setLongitude(-22.77)
+                                     .build();
+
+    assertThat(ServletUtil.extractFromRequestBody(request, gson, PlaceVisitModel.class))
+        .hasValue(testPlace1);
+  }
+
+  @Test
+  public void parseUri_noMatchesTripNamePattern_throwsError() {
+    when(request.getRequestURI()).thenReturn("http://tripmeout.com/trip/abc123");
+
+    assertThrows(
+        IllegalArgumentException.class, () -> ServletUtil.parseUri(request, TRIP_URI_PATTERN));
+  }
+
+  @Test
+  public void parseUri_matchesTripNamePattern_matcherHasCorrectGroup() {
+    when(request.getRequestURI()).thenReturn("http://tripmeout.com/trips/abc123");
+
+    Matcher matcher = ServletUtil.parseUri(request, TRIP_URI_PATTERN);
+
+    assertThat(matcher.matches()).isTrue();
+    assertThat(matcher.group(1)).isEqualTo("abc123");
+  }
+
+  @Test
+  public void parseUri_noMatchesTripAndPlaceNamePattern_throwsError() {
+    when(request.getRequestURI()).thenReturn("http://tripmeout.com/trip/abc123");
+
+    assertThrows(
+        IllegalArgumentException.class, () -> ServletUtil.parseUri(request, PLACE_URI_PATTERN));
+  }
+
+  @Test
+  public void parseUri_matchesTripAndPlaceNamePattern_matcherHasCorrectGroups() {
+    when(request.getRequestURI()).thenReturn("http://tripmeout.com/trips/abc123/places/123");
+
+    Matcher matcher = ServletUtil.parseUri(request, PLACE_URI_PATTERN);
+
+    assertThat(matcher.matches()).isTrue();
+    assertThat(matcher.group(1)).isEqualTo("abc123");
+    assertThat(matcher.group(2)).isEqualTo("123");
+  }
 }

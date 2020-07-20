e74c9e36d370225a3e4b453847bde1cf8552275c
Cindy Li <cindy_li@brown.edu>
2020-07-20 13:55:00 +0000

    fix storage module name in guice context listener

--- frontend/src/main/java/com/google/tripmeout/frontend/FrontendGuiceServletContextListener.java
+++ frontend/src/main/java/com/google/tripmeout/frontend/FrontendGuiceServletContextListener.java
@@ -5,7 +5,7 @@ import com.google.inject.Injector;
 import com.google.inject.servlet.GuiceServletContextListener;
 import com.google.tripmeout.frontend.serialization.GsonModelSerializationModule;
 import com.google.tripmeout.frontend.servlet.ServletsModule;
-import com.google.tripmeout.frontend.storage.StorageBindingModule;
+import com.google.tripmeout.frontend.storage.InMemoryStorageBindingModule;
 
 /**
  * Provides an {@link Injector} configured to serve the frontend application routes.
@@ -16,6 +16,6 @@ public class FrontendGuiceServletContextListener extends GuiceServletContextList
   @Override
   protected Injector getInjector() {
     return Guice.createInjector(
-        new GsonModelSerializationModule(), new ServletsModule(), new StorageBindingModule());
+        new GsonModelSerializationModule(), new ServletsModule(), new InMemoryStorageBindingModule());
   }
 }

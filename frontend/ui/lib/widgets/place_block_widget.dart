fa2dd55a18bbb1e2cde5d18dac2d6ca3c1f4c4a4
anfee1 <afeenster@google.com>
2020-07-20 20:43:33 +0000

    Adding M T D toggle buttons for the places.

--- frontend/ui/lib/widgets/place_block_widget.dart
+++ frontend/ui/lib/widgets/place_block_widget.dart
@@ -13,13 +13,32 @@ class _PlaceBlockWidgetState extends State<PlaceBlockWidget> {
   final String placeName;
   _PlaceBlockWidgetState(this.placeName);
 
+  List<bool> _selections = List.generate(3, (_) => false);
+
   @override
   Widget build(BuildContext context) {
     return ExpansionTile(
         title: Text(placeName),
-        trailing: RaisedButton(
-          onPressed: () => {},
-          child: Text('M T D'),
+        trailing: ToggleButtons(
+          children: <Widget>[
+            Text('M'),
+            Text('T'),
+            Text('D'),
+          ],
+          onPressed: (int index) {
+            setState(() {
+              for (int buttonIndex = 0;
+                  buttonIndex < _selections.length;
+                  buttonIndex++) {
+                if (buttonIndex == index) {
+                  _selections[buttonIndex] = true;
+                } else {
+                  _selections[buttonIndex] = false;
+                }
+              }
+            });
+          },
+          isSelected: _selections,
         ),
         children: [
           Column(children: [
@@ -31,12 +50,3 @@ class _PlaceBlockWidgetState extends State<PlaceBlockWidget> {
         ]);
   }
 }
-
-/*decoration: BoxDecoration(
-          border: Border.all(
-            color: Colors.black,
-          ),
-          borderRadius: BorderRadius.all(Radius.circular(5))),
-      margin: const EdgeInsets.all(15.0),
-      padding: const EdgeInsets.all(10.0),
-      */

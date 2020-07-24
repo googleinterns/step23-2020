8179cbdb956a13c882c4357cfd5da60f368c882a
chuckluv <calovett35@gmail.com>
2020-07-24 15:04:41 +0000

    save changes

--- frontend/ui/test/services/rest_api_trip_service_test.dart
+++ frontend/ui/test/services/rest_api_trip_service_test.dart
@@ -1,20 +1,19 @@
-
 import 'package:test/test.dart';
 import 'package:tripmeout/model/trip.dart';
 import 'package:tripmeout/services/rest_api_trip_service.dart';
 
-void main(){
-  group('Empty RestApiTripService',(){
-    var tripService = TripService();
+void main() {
+  group('Empty RestApiTripService', () {
+    var tripService = RestApiTripService();
     test('gettrip returns Null', () async {
       expect(await tripService.getTrip('sometrip'), isNull);
     });
-     test('listTrips returns empty list', () async {
+    test('listTrips returns empty list', () async {
       expect(await tripService.listTrips(), isEmpty);
     });
   });
   group('Populated RestApiTripService', () {
-    var tripService = TripService();
+    var tripService = RestApiTripService();
     var trip1 = Trip(id: '123', name: 'trip1');
     var trip2 = Trip(id: '456', name: 'trip2');
     // do async setup inside a setUp lambda because group lambdas cant be async

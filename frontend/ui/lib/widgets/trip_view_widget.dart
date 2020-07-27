03b44b64a00745ab8a5feae9ed15665d0bbefe5e
anfee1 <afeenster@google.com>
2020-07-27 21:27:04 +0000

    Making the trip view scrollable.

--- frontend/ui/lib/widgets/trip_view_widget.dart
+++ frontend/ui/lib/widgets/trip_view_widget.dart
@@ -22,15 +22,23 @@ class _TripWidgetState extends State<TripViewWidget> {
           borderRadius: BorderRadius.all(Radius.circular(5))),
       margin: const EdgeInsets.all(15.0),
       padding: const EdgeInsets.all(10.0),
-      child: Column(
-        children: [
-          Text("Name of the Trip"),
-          PlaceBlockWidget("Place 1"),
-          PlaceBlockWidget("Place 2"),
-          PlaceBlockWidget("Place 3"),
-          PlaceBlockWidget("Place 4"),
-          PlaceBlockWidget("Place 5"),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Text("Name of the Trip"),
+            PlaceBlockWidget("Place 1"),
+            PlaceBlockWidget("Place 2"),
+            PlaceBlockWidget("Place 3"),
+            PlaceBlockWidget("Place 4"),
+            PlaceBlockWidget("Place 5"),
+            PlaceBlockWidget("Place 6"),
+            PlaceBlockWidget("Place 7"),
+            PlaceBlockWidget("Place 8"),
+            PlaceBlockWidget("Place 9"),
+            PlaceBlockWidget("Place 10"),
+            PlaceBlockWidget("Place 11"),
+          ],
+        ),
       ),
     );
   }

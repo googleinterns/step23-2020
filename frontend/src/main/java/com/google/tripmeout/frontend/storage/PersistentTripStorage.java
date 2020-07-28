bf58a4b77adb615ba643785b99c6de6d230d0289
chuckluv <calovett35@gmail.com>
2020-07-28 18:04:10 +0000

    run formatter

--- frontend/src/main/java/com/google/tripmeout/frontend/storage/PersistentTripStorage.java
+++ frontend/src/main/java/com/google/tripmeout/frontend/storage/PersistentTripStorage.java
@@ -1,94 +1,88 @@
 package com.google.tripmeout.frontend.storage;
- 
-import com.google.tripmeout.frontend.TripModel;
-import com.google.tripmeout.frontend.error.TripAlreadyExistsException;
-import com.google.tripmeout.frontend.error.TripNotFoundException;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.KeyFactory.Builder;
-import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.PreparedQuery;
- 
+import com.google.appengine.api.datastore.Query;
+import com.google.tripmeout.frontend.TripModel;
+import com.google.tripmeout.frontend.error.TripAlreadyExistsException;
+import com.google.tripmeout.frontend.error.TripNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PersistentTripStorage implements TripStorage {
- 
-   private static final String TRIP_KIND_NAME = "trips";
-   private static final String TRIP_ID_PROPERTY_NAME= "tripId";
-   private static final String USER_ID_PROPERTY_NAME= "userId";
-   private static final String NAME_PROPERTY_NAME= "name";
-   private static final String PLACE_API_ID_PROPERTY_NAME= "placeId";
- 
- public void addTrip(TripModel trip) throws TripAlreadyExistsException{
-   DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-   Key tripKey = KeyFactory.createKey(TRIP_KIND_NAME,trip.id());
-   trip.toBuilder().setId(KeyFactory.keyToString(tripKey));
+  private static final String TRIP_KIND_NAME = "trips";
+  private static final String TRIP_ID_PROPERTY_NAME = "tripId";
+  private static final String USER_ID_PROPERTY_NAME = "userId";
+  private static final String NAME_PROPERTY_NAME = "name";
+  private static final String PLACE_API_ID_PROPERTY_NAME = "placeId";
+
+  public void addTrip(TripModel trip) throws TripAlreadyExistsException {
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    Key tripKey = KeyFactory.createKey(TRIP_KIND_NAME, trip.id());
+    trip.toBuilder().setId(KeyFactory.keyToString(tripKey));
     Entity taskEntity = new Entity(trip.userId(), tripKey);
     taskEntity.setProperty(TRIP_ID_PROPERTY_NAME, trip.id());
     taskEntity.setProperty(USER_ID_PROPERTY_NAME, trip.userId());
     taskEntity.setProperty(NAME_PROPERTY_NAME, trip.name());
     taskEntity.setProperty(PLACE_API_ID_PROPERTY_NAME, trip.placesApiPlaceId());
     datastore.put(taskEntity);
-    
-
- }
- public  void removeTrip(String tripId) throws TripNotFoundException{
+  }
+  public void removeTrip(String tripId) throws TripNotFoundException {
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     Key tripKey = KeyFactory.stringToKey(tripId);
     datastore.delete(tripKey);
- }
- 
- public void updateTripName(String tripId, String tripName) throws TripNotFoundException{
+  }
+
+  public void updateTripName(String tripId, String tripName) throws TripNotFoundException {
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     Key tripKey = KeyFactory.stringToKey(tripId);
-    try{
-    Entity tripEntity = datastore.get(tripKey);
-    tripEntity.setProperty(NAME_PROPERTY_NAME, tripName);
-    datastore.put(tripEntity);
-    }catch(EntityNotFoundException e){
-        throw new TripNotFoundException("Trip with id: " + tripId + " not found");
+    try {
+      Entity tripEntity = datastore.get(tripKey);
+      tripEntity.setProperty(NAME_PROPERTY_NAME, tripName);
+      datastore.put(tripEntity);
+    } catch (EntityNotFoundException e) {
+      throw new TripNotFoundException("Trip with id: " + tripId + " not found");
     }
+  }
 
- }
- 
- public TripModel getTrip(String tripId) throws TripNotFoundException{
+  public TripModel getTrip(String tripId) throws TripNotFoundException {
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     Key tripKey = KeyFactory.stringToKey(tripId);
-    try{
-    Entity tripEntity = datastore.get(tripKey);
-    
-    return TripModel.builder().setId((String)tripEntity.getProperty(TRIP_ID_PROPERTY_NAME)).setName((String)tripEntity.getProperty(NAME_PROPERTY_NAME))
-    .setUserId((String)tripEntity.getProperty(USER_ID_PROPERTY_NAME)).setPlacesApiPlaceId((String)tripEntity.getProperty(PLACE_API_ID_PROPERTY_NAME)).build();
-    
-    }catch(EntityNotFoundException e){
-        throw new TripNotFoundException("Trip with id: " + tripId + " not found");
+    try {
+      Entity tripEntity = datastore.get(tripKey);
+
+      return TripModel.builder()
+          .setId((String) tripEntity.getProperty(TRIP_ID_PROPERTY_NAME))
+          .setName((String) tripEntity.getProperty(NAME_PROPERTY_NAME))
+          .setUserId((String) tripEntity.getProperty(USER_ID_PROPERTY_NAME))
+          .setPlacesApiPlaceId((String) tripEntity.getProperty(PLACE_API_ID_PROPERTY_NAME))
+          .build();
+
+    } catch (EntityNotFoundException e) {
+      throw new TripNotFoundException("Trip with id: " + tripId + " not found");
     }
+  }
 
- }
- 
- public List<TripModel> getAllUserTrips(String userId){
-      Query query =
-        new Query(userId);
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-        PreparedQuery results = datastore.prepare(query);
-        List<TripModel>trips = new ArrayList<>();
-        for(Entity tripEntity: results.asIterable()){
-            
-            trips.add(TripModel.builder()
-            .setId((String)tripEntity.getProperty(TRIP_ID_PROPERTY_NAME))
-            .setName((String)tripEntity.getProperty(NAME_PROPERTY_NAME))
-            .setUserId((String)tripEntity.getProperty(USER_ID_PROPERTY_NAME))
-            .setPlacesApiPlaceId((String)tripEntity.getProperty(PLACE_API_ID_PROPERTY_NAME))
-            .build());
-        }
-        return trips;
-
- }
-  
+  public List<TripModel> getAllUserTrips(String userId) {
+    Query query = new Query(userId);
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+    List<TripModel> trips = new ArrayList<>();
+    for (Entity tripEntity : results.asIterable()) {
+      trips.add(
+          TripModel.builder()
+              .setId((String) tripEntity.getProperty(TRIP_ID_PROPERTY_NAME))
+              .setName((String) tripEntity.getProperty(NAME_PROPERTY_NAME))
+              .setUserId((String) tripEntity.getProperty(USER_ID_PROPERTY_NAME))
+              .setPlacesApiPlaceId((String) tripEntity.getProperty(PLACE_API_ID_PROPERTY_NAME))
+              .build());
+    }
+    return trips;
+  }
 }
--- frontend/src/test/java/com/google/tripmeout/frontend/storage/PersistentTripStorageTest.java
+++ frontend/src/test/java/com/google/tripmeout/frontend/storage/PersistentTripStorageTest.java
@@ -1,7 +1,7 @@
 package com.google.tripmeout.frontend.storage;
 
-import static com.google.common.truth.Truth.assertThat;
 import static com.google.appengine.api.datastore.FetchOptions.Builder.withLimit;
+import static com.google.common.truth.Truth.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
@@ -10,18 +10,16 @@ import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.Query;
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 import com.google.tripmeout.frontend.TripModel;
 import com.google.tripmeout.frontend.error.TripAlreadyExistsException;
 import com.google.tripmeout.frontend.error.TripNotFoundException;
-import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
-import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class PersistentTripStorageTest {
-
   public static final TripModel LONDON_TRIP1 = TripModel.builder()
                                                    .setId("a")
                                                    .setName("London Trip")
@@ -55,9 +53,9 @@ public class PersistentTripStorageTest {
                                                                .setName("London Trip")
                                                                .setUserId("person 2")
                                                                .setPlacesApiPlaceId("place-id")
-                                                               .build();   
+                                                               .build();
 
-    private final LocalServiceTestHelper helper =
+  private final LocalServiceTestHelper helper =
       new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
 
   @Before
@@ -69,13 +67,13 @@ public class PersistentTripStorageTest {
   public void tearDown() {
     helper.tearDown();
   }
-                                                               
+
   @Test
-  public void addTrip_getTrip()throws TripAlreadyExistsException, TripNotFoundException{
+  public void addTrip_getTrip() throws TripAlreadyExistsException, TripNotFoundException {
     PersistentTripStorage storage = new PersistentTripStorage();
     storage.addTrip(TOKYO_TRIP1);
     storage.addTrip(LONDON_TRIP1);
     TripModel gottenTrip = storage.getTrip(LONDON_TRIP1.id());
     assertThat(gottenTrip).isEqualTo(LONDON_TRIP1);
-  }                                                             
+  }
 }

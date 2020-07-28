3726dd08e98501098c9e47e34922a40063c2ed73
cli1903 <cinli@google.com>
2020-07-28 20:21:16 +0000

    integrate place_visit_service

--- frontend/ui/lib/main.dart
+++ frontend/ui/lib/main.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:tripmeout/router/router.dart';
 import 'package:tripmeout/services/rest_api_trip_service.dart';
+import 'package:tripmeout/services/rest_api_place_visit_service.dart';
 import 'package:tripmeout/services/trip_service.dart';
 import 'package:tripmeout/services/in_memory_trip_service.dart';
 import 'package:tripmeout/services/trip_service.dart';
+import 'package:tripmeout/services/place_visit_service.dart';
 import 'package:tripmeout/themes/default_theme.dart';
 
 void main() {
   TripService tripService = RestApiTripService();
-  Router router = Router(tripService);
-  runApp(TripMeOut(tripService, router));
+  PlaceVisitService placeVisitService = RestApiPlaceVisitService();
+  Router router = Router(tripService, placeVisitService);
+  runApp(TripMeOut(tripService, placeVisitService, router));
 }
 
 class TripMeOut extends StatelessWidget {
   TripService tripService;
+  PlaceVisitService placeVisitService;
   Router router;
 
-  TripMeOut(this.tripService, this.router);
+  TripMeOut(this.tripService, this.placeVisitService, this.router);
 
   @override
   Widget build(BuildContext context) {

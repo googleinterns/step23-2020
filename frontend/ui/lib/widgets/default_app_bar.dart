2db9335e6f216eb262fc6265d36dbb5449aff27e
anfee1 <afeenster@google.com>
2020-07-31 19:00:17 +0000

    Getting rid of the app bar on trip list page to avoid future errors.

--- frontend/ui/lib/widgets/default_app_bar.dart
+++ frontend/ui/lib/widgets/default_app_bar.dart
@@ -4,9 +4,20 @@ import 'package:tripmeout/widgets/routing_button_widget.dart';
 
 AppBar defaultAppBar(BuildContext context) {
   return AppBar(
-    // TODO: Customize me yo.
     title: Row(children: [Icon(Icons.airplanemode_active), Text('TripMeOut')]),
-    // Don't show app bar back button, it looks gross.
+    actions: [
+      RoutingButton(
+          Router.createTripRoute, Icon(Icons.add), "Click to make New Trip"),
+      RoutingButton(Router.tripListRoute, Icon(Icons.assignment),
+          "Click to view your trips,")
+    ],
+  );
+}
+
+AppBar defaultAppBarWithNoBackArrow(BuildContext context) {
+  return AppBar(
+    title: Row(children: [Icon(Icons.airplanemode_active), Text('TripMeOut')]),
+    automaticallyImplyLeading: false, //Gets rid of appBar back arrow
     actions: [
       RoutingButton(
           Router.createTripRoute, Icon(Icons.add), "Click to make New Trip"),

7b4c1c26916f8c8a37f5d8a546874b894ab4b6d2
cli1903 <cinli@google.com>
2020-08-06 01:36:36 +0000

    fix place list widget to use picture of place

--- frontend/ui/lib/widgets/place_list_widget.dart
+++ frontend/ui/lib/widgets/place_list_widget.dart
@@ -12,10 +12,11 @@ typedef _OnLoad = Future<List<PlaceBlockWidget>> Function();
 class PlaceListFromServiceWidget extends StatelessWidget {
   final Trip trip;
   final String pageName;
+  final PlacesApiServices placesApiServices;
   final _OnLoad getPlaceBlockWidgets;
 
   PlaceListFromServiceWidget(
-      this.trip, this.pageName, this.getPlaceBlockWidgets);
+      this.trip, this.pageName, this.placesApiServices, this.getPlaceBlockWidgets);
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +24,7 @@ class PlaceListFromServiceWidget extends StatelessWidget {
       future: getPlaceBlockWidgets.call(),
       builder: (context, snapshot) {
         if (snapshot.hasData) {
-          return PlaceListWidget(trip, pageName, snapshot.data);
+          return PlaceListWidgetWithImage(trip, pageName, placesApiServices, snapshot.data);
         }
         if (snapshot.hasError) {
           Scaffold.of(context).showSnackBar(SnackBar(
@@ -41,32 +42,68 @@ class PlaceListFromServiceWidget extends StatelessWidget {
   }
 }
 
-class PlaceListWidget extends StatelessWidget {
+class PlaceListWidgetWithImage extends StatelessWidget {
   final Trip trip;
   final String pageName;
+  final PlacesApiServices placesApiServices;
   final List<PlaceBlockWidget> placeBlocks;
 
-  PlaceListWidget(this.trip, this.pageName, this.placeBlocks);
+  PlaceListWidgetWithImage(this.trip, this.pageName, this.placesApiServices, this.placeBlocks);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Theme.of(context).scaffoldBackgroundColor,
-      child: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            pinned: true,
-            floating: true,
-            snap: true,
-            expandedHeight: 250.0,
-            automaticallyImplyLeading: false, //Gets rid of appBar back arrow
-            flexibleSpace: FlexibleSpaceBar(
-              title: Text(pageName),
-              background: Image.network(
-                'https://www.gannett-cdn.com/presto/2019/02/01/USAT/2af52e69-3fd1-4438-99d7-487a9b51d03c-GettyImages-878868924.jpg',
-                fit: BoxFit.cover,
-              ), //Everybackground is seattle
+    return FutureBuilder(
+      future: placesApiServices.getGoodBannerPhoto(trip.placesApiPlaceId),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          if (snapshot.data == null) {
+            return PlaceListWidget(trip, pageName, 'https://www.gannett-cdn.com/presto/2019/02/01/USAT/2af52e69-3fd1-4438-99d7-487a9b51d03c-GettyImages-878868924.jpg', placeBlocks);
+          }
+          return PlaceListWidget(trip, pageName, snapshot.data, placeBlocks);
+        }
+        if (snapshot.hasError) {
+          Scaffold.of(context).showSnackBar(SnackBar(
+            content: Text("Error getting trip"),
+            action: SnackBarAction(
+              label: "Retry",
+              onPressed: () {}, //TODO: Make retry button actually work.
             ),
+          ));
+          return Container();
+        }
+        return CircularProgressIndicator();
+      },
+    );
+  }
+
+
+}
+
+class PlaceListWidget extends StatelessWidget {
+  final Trip trip;
+  final String pageName;
+  final String photoUrl;
+  final List<PlaceBlockWidget> placeBlocks;
+
+  PlaceListWidget(this.trip, this.pageName, this.photoUrl, this.placeBlocks);
+
+  @override
+  Widget build(BuildContext context) {
+    print(photoUrl);
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          pinned: true,
+          floating: true,
+          snap: true,
+          expandedHeight: 250.0,
+          automaticallyImplyLeading: false, //Gets rid of appBar back arrow
+          flexibleSpace: FlexibleSpaceBar(
+            title: Text(pageName),
+            background: Image.network(
+              photoUrl,
+              fit: BoxFit.cover,
+            ), //Everybackground is seattle
           ),
           SliverList(
             delegate: SliverChildBuilderDelegate(
